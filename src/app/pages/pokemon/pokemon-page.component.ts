e31194b4abe34a385da0eb9e11c2c4d5790626ef
Jose Ramirez <jaro.petroaadlab@gmail.com>
2025-10-15 15:07:15 -0600

    final step 2

--- src/app/pages/pokemon/pokemon-page.component.ts
+++ src/app/pages/pokemon/pokemon-page.component.ts
@@ -1,22 +1,58 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { Pokemon } from '../../pokemons/interfaces';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgClass } from '../../../../node_modules/@angular/common/common_module.d-NEF7UaHr';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pokemon-page',
-  imports: [],
+  // imports: [NgClass],
   templateUrl: './pokemon-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonPageComponent implements OnInit{
+export default class PokemonPageComponent implements OnInit {
   private pokemonService = inject(PokemonsService);
   private route = inject(ActivatedRoute);
   public pokemon = signal<Pokemon | null>(null);
+  private title = inject(Title);
+  private meta = inject(Meta);
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (!id) return;
-    this.pokemonService.loadPokemon(id).subscribe(this.pokemon.set);
+    this.pokemonService
+      .loadPokemon(id)
+      .pipe(
+        tap(({ name, id }) => {
+          const pageTitle = `#${id} - ${name} `;
+          const pageDescription = `Página del Pokémon ${name}`;
+          this.title.setTitle(pageTitle);
+          this.meta.updateTag({
+            name: 'description',
+            content: pageDescription,
+          });
+          this.meta.updateTag({
+            name: 'og:title',
+            content: pageTitle,
+          });
+          this.meta.updateTag({
+            name: 'og:description',
+            content: pageDescription,
+          });
+          this.meta.updateTag({
+            name: 'og:image',
+            content: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+          });
+        })
+      )
+      .subscribe(this.pokemon.set);
   }
 }
