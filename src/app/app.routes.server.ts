ef6a295730493ddd33787fb46126f1ddf36cbd93
Jose Ramirez <jaro.petroaadlab@gmail.com>
2025-10-15 15:23:33 -0600

    final step 2.1

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Server, // render en tiempo real, no prerender
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
